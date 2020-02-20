420028b9e854720d32df4dacef847ed7947b9e60
Svetlana <svetochkaufo@yandex.ru>
2020-02-20 15:33:43 +0300

    feat: add solution typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length ==0 ) return 0;
+  else {
+      array.sort((preview, next) => {
+          if(preview == next) return 0;
+          if(preview > next) return 1;
+          if(preview < next) return -1;
+      });
+      return array[0];
+  };
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array === undefined || array.length ==0 ) return 0;
+    else {
+        array.sort((preview, next) => {
+            if(preview == next) return 0;
+            if(preview > next) return -1;
+            if(preview < next) return 1;
+        });
+        return array[0];
+    };
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array === undefined || array.length ==0 ) return 0;
+    else return array.reduce((summ, item) => summ + item, 0) / array.length;
 }
